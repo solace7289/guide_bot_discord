e5d7ab2dafe27e874e0bbbcaa5cf473e0fad7c77
solace7289 <146842238+solace7289@users.noreply.github.com>
2025-04-12 15:41:34 +0700

    add basic slash commands

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,15 +1,15 @@
 //require slashCommandBuilder from discord.js
-const { slashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 // slash commands to show user information
-const userInforCommand = new slashCommandBuilder()
-  .setName('user')
+const userInforCommand = new SlashCommandBuilder()
+  .setName('sofl')
   .setDescription('Get user information');
 
 module.exports = {
   data: userInforCommand,
   async execute(interaction) {
-    await interaction.reply(`This command is run by ${interaction.user.username}, who join at ${interaction.member.joinAt} `);
+    await interaction.reply(`This command is run by ${interaction.user.username}, who join at ${interaction.member.joinedAt} `);
   },
 };
 
--- index.js
+++ index.js
@@ -1,16 +1,80 @@
 
-// require discord.js 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+// require discord.js
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
 
 // import token from config.json 
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
+// new command collection
+client.commands = new Collection();
+
 //
 client.once(Events.ClientReady, readyClient => {
   console.log(`Running ... `);
   console.log(`Logged in as: ${readyClient.user.tag}`);
+});
+
+// open and read all file in commands folder
+// get path of commands folder
+const folderPath = path.join(__dirname, "commands");
+
+// read folder commands
+const commandFolder = fs.readdirSync(folderPath);
+
+// continue to read each files in each folders inside commands folder
+for (const folder of commandFolder) {
+  // get path of sub-folder
+  const commandPath = path.join(folderPath, folder);
+
+  // read content inside folder 
+  const commandFiles = fs.readdirSync(commandPath).filter(f => f.endsWith('.js'));
+
+  // for each file command js inside sub-folder, read its content and put into commands collection
+  for(const file of commandFiles) {
+    // get path of command file 
+    const filePath = path.join(commandPath, file);
+
+    // read content 
+    const command = require(filePath);
+
+    //check content of each file
+    if( 'data' in command && 'execute' in command ) {
+      // if have right content then set it into collection 
+      client.commands.set(command.data.name, command);
+      
+      //log
+      console.log(`New command have been set: /${command.data.name}`);
+    } else {
+      // if have error then log into screen
+      console.log(`command at file ${command} is missing data or execute!`);
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
 
 // login to server 
